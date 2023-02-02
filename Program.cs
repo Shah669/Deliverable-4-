a226fe4c3cb633d82db9fc39b8c22c0f7a8a5abe
Shah669 <122481622+Shah669@users.noreply.github.com>
2023-02-02 18:12:30 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿/*
+ Author: SM ZAMAN
+ Date: 02/02/2023
+ Comments: This C# Console application code demostrates the use
+            of arrays after getting input from users
+*/
+
+using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+
+            int number = 25;
+            int[] fibonacci = new int[number];
+
+            // initialize fibonacci series
+            fibonacci[0] = 0;
+            fibonacci[1] = 1;
+
+            for (int i = 0; i < number; i++)
+            {
+                if (i >= 2)
+
+                {
+
+                    fibonacci[i] = fibonacci[i - 1] + fibonacci[i - 2];
+                }
+            }
+            for (int i = 0; i < number; i++)
+            {
+                Console.WriteLine("Fibonacci(" + i + ") = " + fibonacci[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+
+}
